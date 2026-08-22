26138e6fdc40609a96107cc665bc231bbf4832c9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-08-22 17:05:22 +0300

    Added message listener to service worker for START_RUN/STOP_RUN

--- popup/src/background/service-worker.js
+++ popup/src/background/service-worker.js
@@ -1,2 +1,15 @@
-
 console.log("[LinkedIn] service worker запущено");
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "START_RUN") {
+    console.log("[LinkedIn] отримано команду START_RUN");
+    sendResponse({ status: "started" });
+  }
+
+  if (message.type === "STOP_RUN") {
+    console.log("[LinkedIn] отримано команду STOP_RUN");
+    sendResponse({ status: "stopped" });
+  }
+
+  return true
+});
