6b01aedba7ff513700cc25a3503df4f87148a8b0
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-08-22 22:17:06 +0300

    Added findPeopleCards function to content script

--- popup/src/content/content-script.js
+++ popup/src/content/content-script.js
@@ -1,1 +1,8 @@
 console.log("[LinkedIn] content script завантажено");
+
+function findPeopleCards() {
+  return document.querySelectorAll('div[role="listitem"]');
+}
+
+const cards = findPeopleCards();
+console.log(`[LinkedIn] знайдено карток: ${cards.length}`);
